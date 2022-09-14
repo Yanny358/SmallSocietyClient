c5c622e93392eb8598b123f5fb3c1a34ac914cb0
jametl <jametl@ttu.ee>
2022-09-14 17:49:42 +0300

    Added follow ui

--- src/features/profiles/ProfileContent.tsx
+++ src/features/profiles/ProfileContent.tsx
@@ -2,6 +2,8 @@ import { observer } from "mobx-react-lite";
 import React from "react";
 import { Tab } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
+import { useStore } from "../../app/stores/store";
+import ProfileFollowings from "./ProfileFollowings";
 import ProfilePhotos from "./ProfilePhotos";
 
 interface Props {
@@ -9,11 +11,14 @@ interface Props {
 }
 
 export default observer(function ProfileContent({profile}: Props) {
+
+    const {profileStore} = useStore();
+
     const panels = [
         {menuItem: 'Photos', render: () => <ProfilePhotos profile={profile} />},
         {menuItem: 'Events', render: () => <Tab.Pane>Events Content</Tab.Pane>},
-        {menuItem: 'Followers', render: () => <Tab.Pane>Followers Content</Tab.Pane>},
-        {menuItem: 'Following', render: () => <Tab.Pane>Following Content</Tab.Pane>},
+        {menuItem: 'Followers', render: () => <ProfileFollowings />},
+        {menuItem: 'Following', render: () => <ProfileFollowings />},
     ];
 
     return (
@@ -21,6 +26,7 @@ export default observer(function ProfileContent({profile}: Props) {
             menu={{fluid: true, vertical: true}}
             menuPosition='right'
             panes={panels}
+            onTabChange={(e, data) => profileStore.setActiveTab(data.activeIndex)}
         />
     )
 })
--- src/features/profiles/ProfilePage.tsx
+++ src/features/profiles/ProfilePage.tsx
@@ -9,11 +9,14 @@ import ProfileHeader from "./ProfileHeader";
 export default observer(function ProfilePage() {
     const { username } = useParams<{ username: string }>();
     const { profileStore } = useStore();
-    const { loadingProfile, loadProfile, profile } = profileStore;
+    const { loadingProfile, loadProfile, profile, setActiveTab } = profileStore;
 
     useEffect(() => {
         loadProfile(username);
-    }, [loadProfile, username])
+        return () => {
+            setActiveTab(0);
+        }
+    }, [loadProfile, username, setActiveTab])
 
     if (loadingProfile) return <h3>Loading profile...</h3>
 
